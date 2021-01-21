cf6e654025c24a7b204abc6ac340c1886fa64a41
atanas.popatanasov <atanas.popatanasov@progress.com>
2021-01-21 20:18:25 +0200

    remove the debug banner

--- todo/todo_app/lib/main.dart
+++ todo/todo_app/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Todos',
       home: MyHomePage(title: 'Flutter Todos'),
     );
