c30185282b0e23350a38e164cfec961f3389f6ba
atanas.popatanasov <atanas.popatanasov@progress.com>
2021-04-28 19:26:18 +0300

    CustomPaint and CustomSingleChildLayout use to create Border

--- render_objects/lib/main.dart
+++ render_objects/lib/main.dart
@@ -35,16 +35,118 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: RadBorder(
-          color: Colors.orange,
-          thickness: 10.0,
-          child: Text(
-            'You have pushed the button this many times:',
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: FlutterBorder(
+            child: Text(
+              'You have pushed the button this many times:',
+            ),
           ),
+        ),
+        // RadBorder(
+        //   color: Colors.orange,
+        //   thickness: 10.0,
+        //   child: Text(
+        //     'You have pushed the button this many times:',
+        //   ),
+        // ),
+      ),
+    );
+  }
+}
+
+class FlutterBorder extends StatefulWidget {
+  final Color color;
+  final double thickness;
+  final Widget child;
+
+  const FlutterBorder({
+    Key key,
+    this.color = Colors.blue,
+    this.thickness = 10,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  _FlutterBorderState createState() => _FlutterBorderState();
+}
+
+class _FlutterBorderState extends State<FlutterBorder> {
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints.loose(Size(200, 50)),
+      child: CustomPaint(
+        painter: _FlutterBorderPainter(
+          color: widget.color,
+          thickness: widget.thickness,
+        ),
+        child: CustomSingleChildLayout(
+          delegate: _FlutterBorderLayoutDelegate(
+            thickness: widget.thickness,
+          ),
+          child: widget.child,
         ),
       ),
     );
   }
+}
+
+class _FlutterBorderLayoutDelegate extends SingleChildLayoutDelegate {
+  final double thickness;
+
+  _FlutterBorderLayoutDelegate({this.thickness});
+
+  @override
+  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
+    final width = constraints.maxWidth - (thickness * 2);
+    final height = constraints.maxHeight - (thickness * 2);
+    BoxConstraints measureConstraint = BoxConstraints.loose(
+      Size(
+        width > 0 ? width : 0,
+        height > 0 ? height : 0,
+      ),
+    );
+    return measureConstraint;
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    final size = super.getSize(constraints);
+    return size;
+  }
+
+  @override
+  Offset getPositionForChild(Size size, Size childSize) {
+    final childOffset = super.getPositionForChild(size, childSize);
+    final offset = Offset(thickness, thickness) + childOffset;
+    return offset;
+  }
+
+  @override
+  bool shouldRelayout(covariant SingleChildLayoutDelegate oldDelegate) => true;
+}
+
+class _FlutterBorderPainter extends CustomPainter {
+  final Color color;
+  final double thickness;
+
+  _FlutterBorderPainter({this.color, this.thickness});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+    paint.color = color;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = thickness;
+
+    canvas.drawRect(
+      Rect.fromLTWH(0.0, 0.0, size.width, size.height),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
 
 class RadBorder extends SingleChildRenderObjectWidget {
@@ -100,7 +202,15 @@ class _RadBorderRenderObject extends RenderBox
   @override
   void performLayout() {
     if (child != null) {
-      child.layout(constraints.loosen(), parentUsesSize: true);
+      final width = constraints.maxWidth - (_thickness * 2);
+      final height = constraints.maxHeight - (_thickness * 2);
+      BoxConstraints measureConstraint = BoxConstraints.loose(
+        Size(
+          width > 0 ? width : 0,
+          height > 0 ? height : 0,
+        ),
+      );
+      child.layout(measureConstraint, parentUsesSize: true);
       final BoxParentData parentData = child.parentData as BoxParentData;
       parentData.offset = Offset(_thickness, _thickness);
 
